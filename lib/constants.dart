0b4dd677f9a31078779266a833aef270155f690e
Raphael Hacques <raphael.hacques@etu.uca.fr>
2021-04-05 23:29:06 +0200

    Constants of the app with colors, and padding use

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+//Colors
+const AppConstPrimaryColor = Color(0xFF0C9869);
+const AppConstTextColor = Color(0xFF3C4046);
+const AppConstBackgroundCardColor = Color(0xFF3C4046);
+const AppConstBackgroundTextCardColor = Colors.white70;
+const AppConstTextNotificationColor = Color(0xFFEAD81B);
+const AppConstBackgroundColor = Color(0xFFF9F8FD);
+
+
+//Size
+const double kDefaultPadding = 20.0;
